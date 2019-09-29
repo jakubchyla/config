c4a9b17b7754cd1b148ea869b10c9b6d1c09be34
Jakub Chyła <jakchyla@gmail.com>
2019-09-29 14:31:44 +0200

    added script for auto settings options in gnome

---
+++ gnome-settings.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+#--------------
+#remove conflicting keybinds
+#--------------
+for i in {1..9}; do
+	gsettings set org.gnome.shell.keybindings switch-to-application-$i "['']"
+done
+
+
+gsettings set org.gnome.mutter.keybindings switch-monitor "['XF86Display']"
+gsettings set org.gnome.shell.keybindings toggle-message-tray "['<Super>v']"
+
+
+#--------------
+#set keybinds
+#--------------
+
+#workspaces
+for i in {1..10}; do
+
+	if [ $i -eq 10 ]; then
+		gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-$i "['<Super>0']"
+		gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-$i "['<Super><Shift>0']"
+	else
+		gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-$i "['<Super>$i']"
+		gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-$i "['<Super><Shift>$i']"
+	fi
+done
+
+#close window
+gsettings set org.gnome.desktop.wm.keybindings close "['<Alt>F4', '<Super><Shift>q']"
+#fullscreen
+gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Super>f']"
+
