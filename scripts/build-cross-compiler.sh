37e82117b75928d6c183b5e596aabe1eeb37c030
Jakub Chyła <jakchyla@gmail.com>
2020-05-18 16:28:08 +0200

    fixed bug

--- scripts/build-cross-compiler.sh
+++ scripts/build-cross-compiler.sh
@@ -7,11 +7,11 @@ BINUTILS_FILENAME=`echo $BINUTILS_URL | grep -P --only-matching "binutils-\d\.\d
 GCC_URL="https://ftp.gnu.org/gnu/gcc/gcc-9.3.0/gcc-9.3.0.tar.xz"
 GCC_FILENAME=`echo $GCC_URL | grep -P --only-matching "gcc-\d\.\d\.\d" | head -n 1`
 
-if [ -d ~/opt/cross_compiler ];then mkdir -p ~/opt/cross_compiler; fi
+if ! [ -d ~/opt/cross_compiler ];then mkdir -p ~/opt/cross_compiler; fi
 
-PREFIX="$HOME/opt/cross_compiler"
-TARGET="i686-elf"
-PATH="$PREFIX/bin:$PATH"
+export PREFIX="$HOME/opt/cross_compiler"
+export TARGET="i686-elf"
+export PATH="$PREFIX/bin:$PATH"
 
 cd /tmp
 curl -o $BINUTILS_FILENAME.tar.xz $BINUTILS_URL
@@ -32,7 +32,7 @@ cd /tmp
 cd $GCC_FILENAME
 mkdir build; cd build
 ../configure --target=$TARGET --prefix="$PREFIX" --disable-nls \
-    --enable-languages=c,c++ --without-headers
+    --enable-languages=c --without-headers
 make -j `nproc` all-gcc
 make -j `nproc` all-target-libgcc
 make -j `nproc` install-gcc
