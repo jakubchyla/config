271b1d00e798bd34cbb4344cb6f671acd1e20633
Jakub Chyła <jakchyla@gmail.com>
2020-07-04 14:29:27 +0200

    update

--- scripts/build_emacs.sh
+++ scripts/build_emacs.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+BUILD_DIR="$HOME/src/emacs-build"
+
 if [ -f "/usr/bin/dnf" ]; then
     sudo dnf -y builddep emacs
     sudo dnf -y install git autogen
@@ -7,12 +9,15 @@ elif [ -f "/usr/bin/zypper" ]; then
     sudo zypper --non-interactive source-install -d emacs
     sudo zypper --non-interactive install git autogen
 fi
-    git clone https://git.savannah.gnu.org/git/emacs.git /tmp/emacs-build
+    if ! [ -d ~/src ]; then
+        mkdir ~/src
+    fi
+    git clone https://git.savannah.gnu.org/git/emacs.git $BUILD_DIR
     GO_BACK_DIR=`pwd`
-    cd /tmp/emacs-build
+    cd $BUILD_DIR
     ./autogen.sh
     ./configure
     make -j `nproc`
-    make install -j `nproc`
+    #make install -j `nproc`
     cd $GO_BACK_DIR
 
